9da04b63ea8626d8d19bc197deb9dc1b0903ca5b
Maria Schilstra <maria.schilstra@gmail.com>
2018-03-22 08:38:35 +0000

    Added some resources for testing

--- borduuru/borduuru.py
+++ borduuru/borduuru.py
@@ -10,7 +10,7 @@ from PyQt5 import QtGui as gui
 
 import pandas as pd, numpy as np, copy as cp
 import math
-
+import matplotlib.pyplot as plt, matplotlib.image as mpimg
 from PIL import Image, ImageFilter
 
 from PyQt5.Qt import QMainWindow
@@ -18,11 +18,19 @@ from PyQt5.Qt import QMainWindow
 class Main(QMainWindow):
     def __init__(self, ):
         super(Main, self).__init__()
-        x = Image.open("C:\\users\\schilsm\\Desktop\\test.bmp") # load an image from the hard drive
-        ax = np.array(x)
-        print(ax.shape)
-        print(ax[:,:,0])
-        print(np.unique(ax))
+        file_path = widgets.QFileDialog.getOpenFileName(self, 
+        "Open Data File", "", "Image data files (*.bmp);;All files (*.*)")[0]
+        if file_path:
+            x = Image.open(file_path) # load an image from the hard drive
+            ax = np.array(x)
+            print(ax.shape)
+            p0 = np.array([0,0,0])
+            for px in ax[:,:]:
+                if px == p0:
+                    print("Yes")
+                #print(px)
+#             img = mpimg.imread(file_path)
+#             img_plot = plt.imshow(img)
         
 #         original.show() # display both images
 #         blurred.show()    
